714cbc4570ca6bda4eee5352929be00e53be8773
OOJNIM <122461223+OOJNIM@users.noreply.github.com>
2024-08-10 02:05:38 +0900

    Update script.js

--- colorflipper/script.js
+++ colorflipper/script.js
@@ -16,3 +16,22 @@ function bgChange () {
 
 btn.addEventListener("click",bgChange);
 
+//아래처럼 array만들어서 할수도 !  
+//const btn = document.querySelector("button");
+// const colName = document.querySelector("p");
+// const  color = ["red","black","purple","yellow","grey","pink"] ; 
+
+// function random() {
+//     return Math.floor(Math.random()*color.length) ; 
+// }
+
+// function bgChange () {
+//     const rndCol = color[random()];  
+
+//     document.body.style.backgroundColor = rndCol; 
+
+//     colName.innerText = rndCol;
+// }
+
+// btn.addEventListener("click",bgChange);
+
