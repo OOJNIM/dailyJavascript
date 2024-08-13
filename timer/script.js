5fdaf42fc617201d39ecbd6ca4155da9e6b984ae
OOJNIM <122461223+OOJNIM@users.noreply.github.com>
2024-08-13 17:51:10 +0900

    Add files via upload

--- timer/script.js
+++ timer/script.js
@@ -1,48 +1,43 @@
+let count = 0;
 
-const digitElement = document.getElementById("digit"); // type : object
-let digit = Number(digitElement.innerText); 
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){ 
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList ; 
+        if (styles.contains('decrease')){
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0 ; 
+        }
+
+        if (count > 0) {
+            value.style.color = "green"; 
+        }
+        
+        if (count < 0 ) {
+            value.style.color = "red";
+        }
+        
+        if (count === 0 ) {
+            value.style.color = "black"; 
+        }
+        
+
+        value.textContent = count ; 
+
+        
+
+    }) //매개 변수e에 이벤트 객체를 전달받는다. 
 
 
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
-
-
-function checkNumber() {
-    if (digit === 0 ) {
-        digitElement.className = "zero"; 
-    }
-    if (digit > 0 ) {
-        digitElement.className = "positive";
-    } 
-    if (digit < 0 ) {
-        digitElement.className = "negative";
-    } 
 }
-
-
-decrease.addEventListener("click", () => {
-    digit = digit - 1; 
-    digitElement.innerText = digit;
-    checkNumber();
-
-});
-
-reset.addEventListener("click", () => {
-    digit = 0;
-    digitElement.innerText = digit;
-    checkNumber();
-})
-
-increase.addEventListener("click",() => {
-    digit  = digit + 1;
-    digitElement.innerText = digit;
-    checkNumber();
-})
-
-
-// 0 이 되면 digit div 에 zero class 추가 해서 색깔바꾸기 
-// 0 이 아니면 zero class 제거하기 
+);
 
 
 
